f010c98bf15841022b7a0f8667d9b3af12a5ea3b
luismunozse <luismunozse@gmail.com>
2026-01-20 17:16:22 -0300

    fix responsive

--- components/ventas/venta-edit-form.tsx
+++ components/ventas/venta-edit-form.tsx
@@ -239,7 +239,7 @@ export function VentaEditForm({ open, onOpenChange, venta, onSuccess }: VentaEdi
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-3xl max-h-[90vh] overflow-y-auto">
+      <DialogContent className="sm:max-w-3xl max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>
             Editar Venta V{String(venta.numeroVenta).padStart(4, "0")}
